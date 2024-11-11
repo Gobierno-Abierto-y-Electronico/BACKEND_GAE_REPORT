00801368e5ab741f25cce867fc72fd51d188b592
unknown <Administrador@PC-ELECTRO-026.DIGITALGOB.LOCAL>
2024-11-11 13:19:50 -0600

    Se hicieron ajustes

--- src/report/report.controller.js
+++ src/report/report.controller.js
@@ -1,44 +1,61 @@
 import Reporte from './report.model.js';
 
 export const storeReporteData = async (req, res) => {
-    const { name, lastName, number, unidadId, reason, selected, date } = req.body;
-    console.log(name, lastName, number, unidadId, reason, selected, date, "registro de asistencia backend");
-
-    const startOfDay = new Date(date + 'T00:00:00.000Z');
-    const endOfDay = new Date(date + 'T23:59:59.999Z');
+    const { records } = req.body; // Recibir el arreglo de registros del frontend
+    if (!records || records.length === 0) {
+        return res.status(400).json({ error: 'No hay registros para almacenar' });
+    }
 
     try {
+        // Convertir la fecha de los registros a inicio y fin del día
+        const startOfDay = new Date(records[0].date + 'T00:00:00.000Z');
+        const endOfDay = new Date(records[0].date + 'T23:59:59.999Z');
+
+        // Buscar el reporte para el día específico
         let reporte = await Reporte.findOne({
             date: {
                 $gte: startOfDay,
-                $lt: endOfDay
-            }
+                $lt: endOfDay,
+            },
         });
 
-        const nuevoRegistro = { name, lastName, number, unidadId, reason, selected };
-
-        if (reporte) {
-            reporte.reportes.push(nuevoRegistro);
-        } else {
+        // Si el reporte no existe, crearlo
+        if (!reporte) {
             reporte = new Reporte({
                 date: startOfDay,
-                reportes: [nuevoRegistro]
+                reportes: [], // Iniciar un array vacío si el reporte no existe
             });
         }
 
+        // Agregar los registros al reporte
+        records.forEach((record) => {
+            const nuevoRegistro = {
+                user: record.user,
+                date: record.date,
+                time: record.time,
+                status: record.status,
+                ip: record.ip,
+            };
+            // Si el reporte ya existe, solo agregar los registros
+            reporte.reportes.push(nuevoRegistro);
+        });
+
+        // Guardar el reporte con los nuevos registros
         await reporte.save();
 
         res.status(200).json({
-            msg: 'Registro de asistencia almacenado correctamente',
-            data: reporte
+            msg: 'Registros de asistencia almacenados correctamente',
+            data: reporte,
         });
     } catch (error) {
         console.error('Error al almacenar el registro de asistencia:', error);
         res.status(500).json({
-            error: 'Error al almacenar el registro de asistencia'
+            error: 'Error al almacenar el registro de asistencia',
         });
     }
 };
+
+
 
 // Obtener el último reporte
 export const getReporteData = async (req, res) => {
